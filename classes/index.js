c62c48d1baa2b8eb8db859094787afcf4f204946
DavidArthurCole <davidarthurcole23@gmail.com>
2025-09-09 20:37:36 -0400

    Fix some typos, add more docs, add example hash check.

---
+++ classes/index.js
@@ -0,0 +1,39 @@
+import { Archive } from './Archive.js'
+import { ArchiveFile } from './ArchiveFile.js'
+import { Browser } from './Browser.js'
+import { ConditionalSkipReason } from './ConditionalSkipReason.js'
+import { CrawledLink } from './CrawledLink.js'
+import { CrawledPackage } from './CrawledPackage.js'
+import { CrawlerJob } from './CrawlerJob.js'
+import { DownloadLink } from './DownloadLink.js'
+import { DownloadlistSelection } from './DownloadlistSelection.js'
+import { Environment } from './Environment.js'
+import { Event } from './Event.js'
+import { FilePackage } from './FilePackage.js'
+import { Filepath } from './Filepath.js'
+import { HTTPProxy } from './HTTPProxy.js'
+import { LinkgrabberSelection } from './LinkgrabberSelection.js'
+import { LinkInfo } from './LinkInfo.js'
+import { ModifyLock } from './ModifyLock.js'
+import { PackagizerLink } from './PackagizerLink.js'
+
+export {
+    Archive,
+    ArchiveFile,
+    Browser,
+    ConditionalSkipReason,
+    CrawledLink,
+    CrawledPackage,
+    CrawlerJob,
+    DownloadLink,
+    DownloadlistSelection,
+    Environment,
+    Event,
+    FilePackage,
+    Filepath,
+    HTTPProxy,
+    LinkgrabberSelection,
+    LinkInfo,
+    ModifyLock,
+    PackagizerLink,
+}
